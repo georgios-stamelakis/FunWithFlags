1041f058c5bc8fa6594af8f616af0f41ccdd848c
Giorgos Stamelakis <georgios.stamelakis@atos.net>
2024-10-21 14:46:06 +0300

    Initial Commit

---
+++ Fun with flags/Fun_with_flagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_with_flagsApp.swift
+//  Fun with flags
+//
+//  Created by Georgios Stamelakis on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Fun_with_flagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
